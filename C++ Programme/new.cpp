5106cd99aea03faa74a27b6f5c3be64e211914be
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-09 13:58:15 +0000

    changed

---
+++ C++ Programme/new.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    int a,b;
+
+    cout<<"enter a: ";
+    cin>>a;
+
+    cout<<"enter b: ";
+    cin>>b;
+
+    cout<<"sum is: "<<a+b<<endl;
+
+    return 0;
+}
