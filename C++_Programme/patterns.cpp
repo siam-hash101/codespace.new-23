c91b97653e865e2bd775ae77bf1d391c47a10fd6
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-13 12:02:11 +0000

    modified

--- C++_Programme/patterns.cpp
+++ C++_Programme/patterns.cpp
@@ -72,5 +72,17 @@ int main() {
         cout<<endl;
     }
 
+// floyed's triangle
+
+    int count=1;
+
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=i ; j++) {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
