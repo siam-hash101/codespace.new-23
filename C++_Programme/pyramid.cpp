7cdfdda058e11453b3e6d04e275a520bb91f96cb
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-13 10:04:33 +0000

    modified

--- C++_Programme/pyramid.cpp
+++ C++_Programme/pyramid.cpp
@@ -16,5 +16,19 @@ int main() {
         cout<<endl;
     }
 
+    cout<<endl;
+
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=height ; j++) {
+            if(j<height-i) {
+                cout<<" ";
+            }
+            else {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
