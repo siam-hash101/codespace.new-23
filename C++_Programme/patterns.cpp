e78b571985ce758ec09c2ca3f02d17a9543ec308
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-13 11:54:34 +0000

    changed

---
+++ C++_Programme/patterns.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int height;
+
+    cout<<"enter height: ";
+    cin>>height;
+
+// inverted pyramid
+
+    for(int i=height ; i>=1 ; i--) {
+        for(int j=1 ; j<=i ; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+// 180 degree rotated pyramid
+
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=height ; j++) {
+            if(j<=height-i) {
+                cout<<"  ";
+            }else {
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+// normal pyramid
+
+    //for(int i=1 ; i<=height ; i++) {
+    //    for(int j=i ; j<=height ; j++) {
+    //        if(j<=height-i) {
+    //            cout<<" ";
+    //        }else {
+    //            cout<<"* ";
+    //        }
+    //    }
+    //    cout<<endl;
+    //}
+
+// pattern for a rhombus
+
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=height ; j++) {
+            if(j<=height-i) {
+                cout<<"  ";
+            }else {
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+
+    for(int i=height ; i>=1 ; i--) {
+        for(int j=1 ; j<=i ; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+// pyramid number pattern
+
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=i ; j++) {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
