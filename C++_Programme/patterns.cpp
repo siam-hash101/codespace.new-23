cba2323b5029398fe2d428bc21cff8bf3f6263d7
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-14 11:51:07 +0000

    modified

--- C++_Programme/patterns.cpp
+++ C++_Programme/patterns.cpp
@@ -84,5 +84,12 @@ int main() {
         cout<<endl;
     }
 
+    for(int i=1 ; i<=height ; i++) {
+        for(int j=1 ; j<=i ; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
