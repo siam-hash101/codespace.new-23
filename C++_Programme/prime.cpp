f515e76e6b4fb59537374b80c552be887dfdd0d0
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-15 15:51:16 +0000

    new

---
+++ C++_Programme/prime.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+
+    bool flag=0;
+
+    for(int i=2 ; i<=sqrt(n) ; i++) {
+        if(n%2==0) {
+            cout<<"non-prime"<<endl;
+            flag=1;
+            break;
+        }  
+    }
+    
+    if(flag==0) {
+        cout<<"prime"<<endl;
+    }
+
+    return 0;
+}
