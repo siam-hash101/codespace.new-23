0b28f80e9d0cd343ed40540cdaa472d878678202
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-15 11:17:13 +0000

    modified

--- C++_Programme/binaryPattern.cpp
+++ C++_Programme/binaryPattern.cpp
@@ -20,24 +20,24 @@ int main() {
     }
 
     for(int i=1 ; i<=n ; i++) {
-        
-        for(int j=1 ; j<=n-i ; j++) {
+        int space=n-i;
+        for(int j=1 ; j<=space ; j++) {
             cout<<"  ";
         }
         for(int j=1 ; j<=i ; j++) {
+            if((i+j) %2==0) {
+                cout<<"1 ";
+            }else if((i+j) %2!=0) {
+                cout<<"0 ";
+            }
+        }
+        for(int j=2 ; j<=i ; j++) {
             if((i+j)%2==0) {
                 cout<<"1 ";
             }else if((i+j)%2!=0) {
                 cout<<"0 ";
             }
         }
-        //for(int j=2 ; j<=i ; j++) {
-        //    if((i+j)%2==0) {
-        //        cout<<"1 ";
-        //    }else if((i+j)%2!=0) {
-        //        cout<<"0 ";
-        //    }
-        //}
         cout<<endl;
     }
 
