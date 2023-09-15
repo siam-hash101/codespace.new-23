7bb63449491929d0cbb12a165c2c7545960bd36e
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-15 14:17:07 +0000

    modified

--- C++_Programme/new.cpp
+++ C++_Programme/new.cpp
@@ -4,23 +4,43 @@ using namespace std;
 
 int main(void) {
 
-    int rows,cols;
+    //int rows,cols;
+//
+//
+    //cout<<"enter row: ";
+    //cin>>rows;
+//
+    //cout<<"enter column: ";
+    //cin>>cols;
+//
+    //for(int i=0 ; i<rows ; i++) {
+    //    for(int j=0 ; j<cols ; j++) {
+    //        cout<<"*";
+    //    }
+    //    cout<<endl;
+    //}
+    //cout<<"\n";
+    //cout<<"\n";
+//
+    int n;
 
+    cin>>n;
 
-    cout<<"enter row: ";
-    cin>>rows;
-
-    cout<<"enter column: ";
-    cin>>cols;
-
-    for(int i=0 ; i<rows ; i++) {
-        for(int j=0 ; j<cols ; j++) {
-            cout<<"*";
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j<=n-i ; j++) {
+            cout<<"  ";
+        }
+        for(int j=1 ; j<=n ; j++) {
+            cout<<"* ";
+        }
+        for(int j=2 ; j<=i ; j++) {
+            cout<<"* ";
+        }
+        for(int j=2 ; j<=i ; j++) {
+            cout<<"* ";
         }
         cout<<endl;
     }
-    cout<<"\n";
-    cout<<"\n";
 
     return 0;
 }
