2d3a3b355c40cd2dd8e336b659708da1f14ca738
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-15 12:23:25 +0000

    modified

--- C++_Programme/binaryPattern.cpp
+++ C++_Programme/binaryPattern.cpp
@@ -22,7 +22,7 @@ int main() {
     for(int i=1 ; i<=n ; i++) {
         int space=n-i;
         for(int j=1 ; j<=space ; j++) {
-            cout<<"  ";
+            cout<<" ";
         }
         for(int j=1 ; j<=i ; j++) {
             if((i+j) %2==0) {
@@ -31,15 +31,35 @@ int main() {
                 cout<<"0 ";
             }
         }
-        for(int j=2 ; j<=i ; j++) {
-            if((i+j)%2==0) {
-                cout<<"1 ";
-            }else if((i+j)%2!=0) {
-                cout<<"0 ";
-            }
+        cout<<endl;
+    }
+
+cout<<endl;
+
+// rhombus pattern
+
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j<=n-i ; j++) {
+            cout<<"  ";
+        }
+        for(int j=1 ; j<=n ; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+cout<<endl;
+// number pyramid pattern
+
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j<=n-i ; j++) {
+            cout<<" ";
+        }
+        for(int j=1 ; j<=i ; j++) {
+            cout<<j<<" ";
         }
         cout<<endl;
     }
 
     return 0;
-}
+    }
