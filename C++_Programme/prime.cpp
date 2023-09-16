53ac6f990066cd0cc57dcc81a4fe3117f9b5892c
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-16 12:11:20 +0000

    modified

--- C++_Programme/prime.cpp
+++ C++_Programme/prime.cpp
@@ -2,9 +2,18 @@
 #include<cmath>
 using namespace std;
 
+bool isPrime(int num) {
+    for(int i=2 ; i<=sqrt(num) ; i++) {
+        if(num%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
 
-    int n;
+/*    int n;
     cout<<"enter number: ";
     cin>>n;
 
@@ -20,6 +29,19 @@ int main() {
     
     if(flag==0) {
         cout<<"prime"<<endl;
+    }
+*/
+
+    int a,b;
+    cout<<"enter num1: ";
+    cin>>a;
+    cout<<"enter num2: ";
+    cin>>b;
+
+    for(int i=a ; i<=b ; i++) {
+        if(isPrime(i)) {
+            cout<<i<<endl;
+        }
     }
 
     return 0;
