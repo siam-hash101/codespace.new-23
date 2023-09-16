05c010e72fac2ed824893661b770c13bd17500ec
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-16 11:09:36 +0000

    modified

--- C++_Programme/reverse.cpp
+++ C++_Programme/reverse.cpp
@@ -10,8 +10,8 @@ int main() {
     int reverse=0;
 
     while(n>0) {
-        int lastDigit=n%10;
-        reverse=reverse*10+lastDigit;
+        int lastDigit = n%10;
+        reverse = reverse*10 + lastDigit;
         n=n/10;
     }
 
