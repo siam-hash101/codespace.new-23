734d54d09ad826b182031c0d601122537e92d4d2
Ehsanul Siam <137838629+siam-hash101@users.noreply.github.com>
2023-09-23 11:13:07 +0000

    new

---
+++ C++_Programme/sumOfN.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n) {
+    int result=0;
+    for(int i=1 ; i<=n ; i++) {
+        result+=i;
+    }
+    return result;
+}
+int main() {
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    cout<<sum(n)<<endl;
+}
